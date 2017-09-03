252b60acd0280e3353c4cba3d2f9056f55ec6ab4
Spencer Gabhart <sgabhart2230@gmail.com>
2017-09-03 09:00:10 -0400

    New event handler (buttons)

--- app/src/main/java/com/sgabhart/xsandos/MainActivity.java
+++ app/src/main/java/com/sgabhart/xsandos/MainActivity.java
@@ -3,8 +3,10 @@ package com.sgabhart.xsandos;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.graphics.Point;
+import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     private Button[][] buttons;
@@ -29,14 +31,33 @@ public class MainActivity extends AppCompatActivity {
 
         // Create buttons, add to layout
         buttons = new Button[TicTacToe.SIDE][TicTacToe.SIDE];
+        ButtonHandler bh = new ButtonHandler();
         for (int row = 0; row < TicTacToe.SIDE; row++) {
             for (int col = 0; col < TicTacToe.SIDE; col++) {
                 buttons[row][col] = new Button(this);
+                buttons[row][col].setTextSize((int)(.2 * w));
+                buttons[row][col].setOnClickListener(bh);
                 gl.addView(buttons[row][col], w, w);
             }
         }
 
         // Set gl as contentView
         setContentView(gl);
-    }
+    } // buildGuiByCode
+
+    public void update(int row, int column) {
+        Log.w("MainActivity", "Inside update: " + row + ", " + column);
+        buttons[row][column].setText("X");
+    } //update
+
+    private class ButtonHandler implements View.OnClickListener {
+        public void onClick(View v) {
+            Log.w("MainActivity", "Inside onClck, v = " + v);
+            for (int row = 0; row < TicTacToe.SIDE; row++) {
+                for (int col = 0; col < TicTacToe.SIDE; col++) {
+                    if(v == buttons[row][col]) update(row, col);
+                }
+            }
+        } // onCLick
+    } // ButtonHandler
 }
