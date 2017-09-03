dfc96cc99db29c39b5293e5984e2061c79b9b7d8
Spencer Gabhart <sgabhart2230@gmail.com>
2017-09-03 08:38:24 -0400

    GUI builder added

--- app/src/main/java/com/sgabhart/xsandos/MainActivity.java
+++ app/src/main/java/com/sgabhart/xsandos/MainActivity.java
@@ -2,12 +2,41 @@ package com.sgabhart.xsandos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.graphics.Point;
+import android.widget.Button;
+import android.widget.GridLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private Button[][] buttons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // setContentView(R.layout.activity_main);
+        buildGuiByCode();
+    }
+
+    public void buildGuiByCode() {
+        // Get width of screen
+        Point size = new Point();
+        getWindowManager().getDefaultDisplay().getSize(size);
+        int w = size.x / TicTacToe.SIDE;
+
+        // Create layout manager
+        GridLayout gl = new GridLayout(this);
+        gl.setColumnCount(TicTacToe.SIDE);
+        gl.setRowCount(TicTacToe.SIDE);
+
+        // Create buttons, add to layout
+        buttons = new Button[TicTacToe.SIDE][TicTacToe.SIDE];
+        for (int row = 0; row < TicTacToe.SIDE; row++) {
+            for (int col = 0; col < TicTacToe.SIDE; col++) {
+                buttons[row][col] = new Button(this);
+                gl.addView(buttons[row][col], w, w);
+            }
+        }
+
+        // Set gl as contentView
+        setContentView(gl);
     }
 }
